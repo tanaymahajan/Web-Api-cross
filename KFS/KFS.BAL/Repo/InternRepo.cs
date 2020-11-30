20717a53d88893140d485489afeb7d7760b806c6
Aman Raut <amanraut46@gmail.com>
2020-11-30 12:29:16 +0530

    Repo create

---
+++ KFS/KFS.BAL/Repo/InternRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KFS.BAL.interfaces;
+
+namespace KFS.BAL.Repo
+{
+    public class InternRepo : IInterns
+    {
+        public int AddInter(InternInfo model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<InternInfo> GetAllIntern()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KFS/KFS.BAL/interfaces/IInterns.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFS.BAL.interfaces
+{
+    public interface IInterns
+    {
+        List<InternInfo> GetAllIntern();
+        int AddInter(InternInfo model);
+    }
+}
